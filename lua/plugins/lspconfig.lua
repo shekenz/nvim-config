69d6bc244bad4a038d2b18247e72dd3270184973
shekenz <2657910+shekenz@users.noreply.github.com>
2024-09-17 22:05:49 +0200

    Added tailwindcss lsp-config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -63,6 +63,10 @@ return {
       }
     }
 
+    lspconfig.zls.setup({})
+
+    lspconfig.tailwindcss.setup{}
+
     vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
       -- Disable underline, it's very annoying
       underline = false,
